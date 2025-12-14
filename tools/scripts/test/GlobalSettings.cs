f384c195e4958f2dc9cf4add0c8cad405942a1fe
rory-cd <rory.doug@gmail.com>
2025-12-15 00:46:03 +1100

    FIX: Add newline to end of files

--- tools/scripts/test/GlobalSettings.cs
+++ tools/scripts/test/GlobalSettings.cs
@@ -3,5 +3,5 @@ public static class GlobalSettings
     // This path references resources used by the SplashKit Core integration test suite.
     // By referencing the below field, NuGet test projects are able to share assets with the core tests.
     // To utilise, put "SetResourcesPath(GlobalSettings.ResourcePath);" at the beginning of a test project
-    public static string ResourcePath = "../../../../coresdk/src/test/Resources";
+    public static readonly string ResourcePath = "../../../../coresdk/src/test/Resources";
 }
